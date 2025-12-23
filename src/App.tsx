5650748ea0fdc54dfcee34ffb7b75305fe570abb
ArsenZar <arsenzar@gmail.com>
2025-12-23 03:14:06 +0200

    create own hook (incapsulation query find post)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { useWindowWidth } from './hooks/useWindowWidth';
+import { useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+// 1. Імпортуємо хук
+import { useFetchPosts } from './hooks/useFetchPosts';
 
 export default function App() {
-  const windowWidth = useWindowWidth();
+  const [searchQuery, setSearchQuery] = useState('');
 
-  return <p>Current window width: {windowWidth}px</p>;
-};
+  // 2. Використовуємо хук
+  const { data: posts, isFetching } = useFetchPosts(searchQuery);
+
+  const updateSearchQuery = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value),
+    300
+  );
+
+  return (
+    <>
+      <input
+        type="text"
+        defaultValue={searchQuery}
+        onChange={updateSearchQuery}
+        placeholder="Search posts"
+      />
+      {isFetching && <div>Loading posts...</div>}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
