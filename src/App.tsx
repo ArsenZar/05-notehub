1dccdd3c0619ecdce97615d2e597263cf317f318
ArsenZar <arsenzar@gmail.com>
2025-12-23 03:07:20 +0200

    create own hook (show device width)

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,7 @@
-import { useState } from 'react';
-import { useDebouncedCallback } from 'use-debounce';
-import axios from 'axios';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
-
-interface FetchPostsResponse {
-  posts: Post[];
-}
-
-export const fetchPosts = async (searchText: string) => {
-  const res = await axios.get<FetchPostsResponse>(
-    'https://dummyjson.com/posts/search',
-    {
-      params: {
-        q: searchText,
-      },
-    }
-  );
-  return res.data.posts;
-};
+import { useWindowWidth } from './hooks/useWindowWidth';
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState('');
+  const windowWidth = useWindowWidth();
 
-  const { data: posts, isFetching } = useQuery({
-    queryKey: ['posts', searchQuery],
-    queryFn: () => fetchPosts(searchQuery),
-    placeholderData: keepPreviousData,
-  });
-
-  const updateSearchQuery = useDebouncedCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value),
-    1000
-  );
-
-  return (
-    <>
-      <input
-        type="text"
-        defaultValue={searchQuery}
-        onChange={updateSearchQuery}
-        placeholder="Search posts"
-      />
-      {isFetching && <div>Loading posts...</div>}
-      {posts && (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      )}
-    </>
-  );
-}
+  return <p>Current window width: {windowWidth}px</p>;
+};
