dbef7a36ea6a6102e24209680b4a5dadac3ec4b7
ArsenZar <arsenzar@gmail.com>
2025-12-29 14:21:25 +0200

    add input + useDebaunced

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useState, type ReactElement } from "react";
 import { fetchNotes } from "./services/noteService";
 import { useQuery } from "@tanstack/react-query";
+import type { Note } from "./types/note";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function App() {
 
@@ -13,13 +15,34 @@ export default function App() {
     placeholderData: prev => prev
   });
 
-  const notes = data?.notes;
-  
-  
+  const notes= data?.notes;
+
+  const enterInput = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value.trim();
+    if (query !== "") {
+      setSearch(query);
+      setPage(1);
+    } else {
+      setSearch(undefined);
+      setPage(1);
+    }
+  }, 1000);
 
   return (
     <>
-      
+      <input type="text" name="query" defaultValue={search} onChange={enterInput}/>
+    {isLoading || isFetching && <p>Loading...</p>}
+    {isError && <p>Error...</p>}
+    <ul>
+      { 
+        notes?.map((note) => (
+          <li key={note.id}>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </li>
+        ))
+      }
+      </ul>
     </>
   );
 }
