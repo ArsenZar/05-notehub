7d11f3b290f2f1bcd0df60f2f2f92bc55b5667a3
ArsenZar <arsenzar@gmail.com>
2026-01-05 13:45:46 +0200

    NoteList

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,48 @@
+import type { Note } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
+import css from "./NoteList.module.css";
+import { useState } from "react";
+
+interface NoteListProps {
+    notes: Note[] | undefined;
+    deletedId?: string;
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+
+    const [deletedId, setDeletedId] = useState<string | null>(null);
+
+      const queryClient = useQueryClient();
+    
+      const mutation = useMutation({
+          mutationFn: deleteNote,
+          onSuccess: () => {
+              queryClient.invalidateQueries({ queryKey: ["notes"] });
+              setDeletedId(null);
+          },
+      });
+
+    return (
+        <ul className={css.list}>
+            {
+                notes?.map((note) => (
+                    <li className={css.listItem} key={note.id}>
+                        <h2 className={css.title}>{note.title}</h2>
+                        <p className={css.content}>{note.content}</p>
+                        <div className={css.footer}>
+                            <span className={css.tag}>{note.tag}</span>
+                            <button className={css.button} onClick={() => {
+                                setDeletedId(note.id);
+                                mutation.mutate(note.id);
+                            }}
+                            >
+                                { deletedId === note.id ? "Deleting..." : "Delete"}
+                            </button>
+                        </div>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
