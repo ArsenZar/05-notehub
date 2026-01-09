a3e83d629f514ac898fe046f565711514f91b471
ArsenZar <arsenzar@gmail.com>
2026-01-09 12:55:50 +0200

    ready edit func 0.1v

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -118,7 +118,7 @@ export default function NoteForm({ onClose, setPage, editingNote }: NoteFormProp
                     <button
                         type="submit"
                         className={css.submitButton}
-                        disabled={mutationCreate.isPending}
+                        disabled={mutationCreate.isPending || mutationEdit.isPending}
                     >
                         {mutationCreate.isPending ? "Creating..." : ( editingNote ? "Save changes" : "Create note" )}
                     </button>
